cea24f2596ced9c6aeefdaeec94d492fa07c180c
Abduljamil <anizomxoja@gmail.com>
2023-05-09 12:35:56 +0500

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Abduljamil SwiftCoder on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
